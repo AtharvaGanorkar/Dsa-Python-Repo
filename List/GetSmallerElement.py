ed03169de345dc93c48ceeff659c7be508516179
AtharvaGanorkar <atharvaganorkar148@gmail.com>
2025-01-24 10:02:46 +0000

    Get Smaller Elements

---
+++ List/GetSmallerElement.py
@@ -0,0 +1,11 @@
+def SmallerElement(l,x):
+    S=[]
+    for y in l:
+        if y<x:
+            S.append(y)
+    return S
+
+l = [8,100,20,40,3,7,10]
+x = 10
+
+print("Smallest elements than",x,"in the given list are",SmallerElement(l,x))
