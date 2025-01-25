3ae2d0595d1f9c56e278d130b8195f2a0aa4c434
AtharvaGanorkar <atharvaganorkar148@gmail.com>
2025-01-25 06:48:12 +0000

    list

---
+++ List/Largest Element in the List.py
@@ -0,0 +1,27 @@
+l = [int(x) for x in input("Enter numbers Seprated by commas: ").split(',')]
+def LargestElement(l):
+    l.sort()
+    return l[-1]
+
+
+print(LargestElement(l))
+
+def getMax(l):
+    for x in l:
+        for y in l:
+            if y > x:
+                break
+            else:
+                return x
+        return None
+    
+
+def getmax(l):
+    if not l:
+        return None
+    else:
+        res = l[0]
+        for i in range(1,len(l)):
+            if l[i] > res:
+                res = l[i]
+        return res
