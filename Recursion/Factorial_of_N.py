e18ed3e9fc5b51de00c2a02b0fa22ce4f8f59f70
AtharvaGanorkar <atharvaganorkar148@gmail.com>
2025-01-31 10:18:44 +0000

    Factorial of N number

--- Recursion/Factorial_of_N.py
+++ Recursion/Factorial_of_N.py
@@ -1,6 +1,11 @@
 def FactN(n,a):
-    if n == 0:
+    if n <= 0:
         return a
     return FactN(n-1,n*a)
 
-print(FactN(4,1))
+print(FactN(5,1))
+
+def FactN(n):
+    if n<= 0:
+        return 1
+    return n * FactN(n-1)
