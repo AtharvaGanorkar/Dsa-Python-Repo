a1a3ee2a58c8003a478933eaca61d81581d143e1
AtharvaGanorkar <atharvaganorkar148@gmail.com>
2025-02-04 11:22:49 +0000

    Recursive Binary Search

---
+++ Binary Search/Recursive_Binary_search.py
@@ -0,0 +1,20 @@
+# +++++++++++Recursive Binary Search +++++++++++++++++
+
+def Bsearch(l,x,low,high):
+    if low > high:
+        return -1
+    mid = (low+high)//2
+    if l[mid] == x:
+        return mid
+    elif l[mid] > x:
+        return Bsearch(l,x,low,mid-1)
+    else:
+        return Bsearch(l,x,mid+1,high)
+
+def BsearchMain(l,x):
+    return Bsearch(l,x,0,len(l)-1)
+
+l =[10,20,30,40,50,60]  
+x = 20
+
+print(BsearchMain(l,x))
