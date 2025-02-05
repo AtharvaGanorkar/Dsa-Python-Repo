068ec053d24fc8ab6e2b269067d3d81df4da5869
AtharvaGanorkar <atharvaganorkar148@gmail.com>
2025-02-05 05:37:28 +0000

    Index of first occurnce in sorted array

---
+++ 03_Binary Search/Index_of_First_occurance.py
@@ -0,0 +1,36 @@
+# ++++++++++++++++++++ RECURSIVE APPROACH ++++++++++++++++++++++++++
+
+def Firstoccurance(arr,low,high,x):
+    if low > high:
+        return -1
+    mid = low + (high - low)//2
+    if arr[mid] > x:
+        return Firstoccurance(arr,low,mid-1,x)
+    elif arr[mid] < x:
+        return Firstoccurance(arr,mid+1,high,x)
+    else:
+        if mid[arr] == 0 or arr[mid-1] != arr[mid]:
+            return mid
+        else:
+            return Firstoccurance(arr,low,mid-1,x)
+        
+
+
+# +++++++++++++++++++++ ITTERATIVE APPROACH +++++++++++++++++++++++++++++++
+
+def FirstOccurance(x):
+    low = 0
+    high = len(arr)-1
+    while low <= high:
+        mid = low + (high - low) // 2
+        if arr[mid] > x:
+            high = mid - 1
+        elif arr[mid] < x:
+            low = mid + 1
+        else:
+            if arr[mid] == 0 or arr[mid-1] != arr[mid]:
+                return mid
+            else:
+                high = mid-1
+    return -1
+
