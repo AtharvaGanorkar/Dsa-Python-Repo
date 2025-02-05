e1b7de665cf3cf704e3421773fefd7e122ec0828
AtharvaGanorkar <atharvaganorkar148@gmail.com>
2025-02-05 11:03:15 +0000

    Peak Element in an array

---
+++ 03_Binary Search/Peak_Element.py
@@ -0,0 +1,85 @@
+# Peak element
+'''Given an array arr[] where no two adjacent elements are same, find the index of a peak element. 
+   An element is considered to be a peak if it is greater than its adjacent elements (if they exist). 
+   If there are multiple peak elements, return index of any one of them. The output will be "true" 
+   if the index returned by your function is correct; otherwise, it will be "false".
+
+Note: Consider the element before the first element and the element after the last element to be negative infinity.
+
+Examples :
+
+Input: arr = [1, 2, 4, 5, 7, 8, 3]
+Output: true
+Explanation: arr[5] = 8 is a peak element because arr[4] < arr[5] > arr[6].
+
+Input: arr = [10, 20, 15, 2, 23, 90, 80]
+Output: true
+Explanation: arr[1] = 20 and arr[5] = 90 are peak elements because 
+arr[0] < arr[1] > arr[2] and arr[4] < arr[5] > arr[6]. 
+
+Input: arr = [1, 2, 3]
+Output: true
+Explanation: arr[2] is a peak element because arr[1] < arr[2] and arr[2] is the last element, 
+so it has negative infinity to its right.'''
+
+
+arr = [10, 20, 15, 2, 23, 90, 80]
+
+
+
+def peakElement(arr):
+        low = 0
+        high = len(arr)-1
+        while low <= high:
+            mid = low + (high - low)// 2
+            if (mid == 0 or arr[mid-1] <= arr[mid]) and (mid == len(arr)-1 
+                or arr[mid+1] <= arr[mid]):
+                    return mid
+            elif mid > 0 and arr[mid-1] > arr[mid]:
+                high = mid - 1
+            else:
+                low = mid + 1
+        return -1
+
+print(peakElement(arr))
+
+def PeakElement(arr):
+      n = len(arr)
+
+      for i in range(n):
+            left = False
+            right = False
+            
+            if i > 0 and arr[i] >= arr[i-1]:
+                 left = True
+            if i < n-1 and arr[i] >= arr[i+1]:
+                  right = True
+            if left == True and right == True:
+                  return 1
+      return -1   
+
+
+print(PeakElement(arr))
+
+def peakElement(arr):
+      n = len(arr)
+      if n == 1:
+            return 0
+      if arr[0] > arr[1]:
+            return 0
+      if arr[n-1] > arr[n-2]:
+            return n-1
+
+      low,high = 1,n-2
+      while low <= high:
+            mid = low + (high -  low) // 2
+            if arr[mid] > arr[mid -1] and arr[mid] > arr[mid + 1]:
+                  return mid
+            if(arr[mid] < arr[mid + 1]):
+                  low = mid + 1
+            else:
+                  high = mid - 1
+      return "no Peak"
+    
+
+print(peakElement(arr))
