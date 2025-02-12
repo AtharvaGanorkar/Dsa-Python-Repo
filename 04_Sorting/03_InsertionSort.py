f00c7bc32f423d55d671f9219213207f2441965a
AtharvaGanorkar <atharvaganorkar148@gmail.com>
2025-02-12 08:05:18 +0000

    Insertion Sort

---
+++ 04_Sorting/03_InsertionSort.py
@@ -0,0 +1,15 @@
+def InsertionSort(l):
+    for i in range(1,len(l)):
+        x = l[i]
+        j = i-1
+        while j >=0 and x < l[j]:
+            l[j+1] = l[j]
+            j = j-1
+        l[j+1] =x
+
+
+l = [20,5,40,60,10,30]
+InsertionSort(l)
+print(l)
+
+# Insertion sort is a stable algorithm
