166807efdcc2880ebcd5a40897aa32913e198b96
AtharvaGanorkar <atharvaganorkar148@gmail.com>
2025-03-08 05:21:36 +0000

    Linear Probing in Hashing

---
+++ 05_Hashing/02_Linear probing in Hashing.py
@@ -0,0 +1,25 @@
+def LinearProbing(arr,hashSize,sizeOfArray):
+    table = [-1 for i in range(hashSize)]
+
+    for i in range(sizeOfArray):
+        index = arr[i] % hashSize
+        if table[index] == -1:
+            table[index] = arr[i]
+
+        else:
+            k = arr[i] % hashSize
+            flag= 0
+            counter = 0
+            while counter < hashSize and table[k] != -1:
+                if table[k] == arr[i]:
+                    flag = 1
+                    break
+                k = (k + 1)%hashSize
+                counter +=1
+
+            if flag == 1:
+                continue
+            if counter < hashSize:
+                table[k] = arr[i]   
+    
+    return table
