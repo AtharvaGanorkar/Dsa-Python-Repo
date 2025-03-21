74341e4f39687c8aef65dab05e59a9d4648a9b2b
AtharvaGanorkar <atharvaganorkar148@gmail.com>
2025-03-21 05:34:27 +0000

    Delete First Node Of Linked List in Python

---
+++ 07_Linked_List/07_Delete First Node Of Linked List in Python.py
@@ -0,0 +1,37 @@
+class Linked_List:
+    def __init__(self,k):
+        self.key = k
+        self.next = None
+
+temp1 = Linked_List(10)
+temp2 = Linked_List(20)
+temp3 = Linked_List(30)
+temp4 = Linked_List(40)
+
+head =  temp1
+temp1.next = temp2
+temp2.next = temp3
+temp3.next = temp4
+
+def Del(head):
+    if head == None:
+        return None
+    else:
+        return head.next
+    
+
+head = Del(head)
+
+def PrintList(head):
+    curr = head
+    while curr != None:
+        print(curr.key,end="->")
+        curr = curr.next
+    print("None")
+
+PrintList(head)
+
+    
+
+
+    
