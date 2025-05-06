5ea779c323c12378a8d833ba8da420c6e63ee98a
AtharvaGanorkar <atharvaganorkar148@gmail.com>
2025-05-06 01:03:24 +0000

    Selection_Sort

--- 04_Sorting/02_SelectionSort.py
+++ 04_Sorting/02_SelectionSort.py
@@ -15,3 +15,18 @@ print(arr)
 
 
 # Selection sort is not a stable sorting algorithm.
+
+
+def SelectionSort(arr):
+    for i in range(len(arr)-1):
+        min_index = i
+        for j in range(i+1,len(arr)):
+            if arr[min_index] > arr[j]:
+                min_index = j
+        arr[i],arr[min_index] = arr[min_index],arr[i]
+
+arr = [10,5,8,20,2,18]
+
+SelectionSort(arr)
+print(arr)
+
