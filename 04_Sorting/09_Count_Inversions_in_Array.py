16cc3ab8daf0a82bd14d32783cfe87b97f6f83e1
AtharvaGanorkar <atharvaganorkar148@gmail.com>
2025-05-06 01:59:31 +0000

    Count Inversion

--- 04_Sorting/09_Count_Inversions_in_Array.py
+++ 04_Sorting/09_Count_Inversions_in_Array.py
@@ -19,46 +19,48 @@ arr = [2,4,1,3,5]
 
 
 
-def inversionCount(arr,l,m,r):
-        res = 0
-        l = 0
-        r = len(arr)-1
-        m = (l + r)//2
-        if ( l < r):
-            m = (l + r)//2
-            
-            res += inversionCount(arr,l,m)
-            res += inversionCount(arr,m+1,r)
-            res += mergecount(arr,l,m,r)
-            
+def merge_and_count(arr, left, mid, right):
+    left_part = arr[left:mid + 1]
+    right_part = arr[mid + 1:right + 1]
     
-def mergecount(arr,l,mid,r):
-        res = 0
-        low = 0
-        mid = (l + r)//2
-        high = len(arr)-1
-        i = 0
-        j = 0
-        k = 0
-        left = arr[low:mid+1]
-        right = arr[mid+1:high+1]
-        count = 0
-        while i < len(left) and j < len(left):
-            if left[i] <= right[j]:
-                i +=1
-            else:
-                arr[k] = left[i]
-                k +=1
-                j +=1
-        while i < len(left):
-            arr[k] = left[i]
-            i +=1
-            k +=1
-        while j < len(right):
-            arr[k] = left[j]
-            j +=1
-            k +=1
-            
-        return count
+    i = j = 0
+    k = left
+    inv_count = 0
 
-print(inversionCount(arr,l,m,r))
+    while i < len(left_part) and j < len(right_part):
+        if left_part[i] <= right_part[j]:
+            arr[k] = left_part[i]
+            i += 1
+        else:
+            arr[k] = right_part[j]
+            inv_count += (len(left_part) - i)  # Count inversions
+            j += 1
+        k += 1
+
+    while i < len(left_part):
+        arr[k] = left_part[i]
+        i += 1
+        k += 1
+
+    while j < len(right_part):
+        arr[k] = right_part[j]
+        j += 1
+        k += 1
+
+    return inv_count
+
+
+def count_inversions(arr, left, right):
+    inv_count = 0
+    if left < right:
+        mid = (left + right) // 2
+        inv_count += count_inversions(arr, left, mid)
+        inv_count += count_inversions(arr, mid + 1, right)
+        inv_count += merge_and_count(arr, left, mid, right)
+    return inv_count
+
+
+# Driver code
+arr = [2, 4, 1, 3, 5]
+inversion_total = count_inversions(arr, 0, len(arr) - 1)
+print(inversion_total)  # Output: 3
