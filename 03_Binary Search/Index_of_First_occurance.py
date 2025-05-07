b4909043ff70ba8531b256ba654e75395ccf8f43
AtharvaGanorkar <atharvaganorkar148@gmail.com>
2025-05-07 07:05:47 +0000

    Index of  First Occurance

--- 03_Binary Search/Index_of_First_occurance.py
+++ 03_Binary Search/Index_of_First_occurance.py
@@ -9,17 +9,19 @@ def Firstoccurance(arr,low,high,x):
     elif arr[mid] < x:
         return Firstoccurance(arr,mid+1,high,x)
     else:
-        if mid[arr] == 0 or arr[mid-1] != arr[mid]:
+        if mid == 0 or arr[mid-1] != arr[mid]:
             return mid
         else:
             return Firstoccurance(arr,low,mid-1,x)
         
 
+        
+
 
 # +++++++++++++++++++++ ITTERATIVE APPROACH +++++++++++++++++++++++++++++++
-arr = [10,30,20,20,30,40]
-x = 10
-def FirstOccurance(x):
+arr = [10,20,20,30,30,30,40]
+x =  30
+def FirstOccurance(arr,x):
     low = 0
     high = len(arr)-1
     while low <= high:
@@ -29,10 +31,11 @@ def FirstOccurance(x):
         elif arr[mid] < x:
             low = mid + 1
         else:
-            if arr[mid] == 0 or arr[mid-1] != arr[mid]:
+            if mid == 0 or arr[mid] != arr[mid-1] :
                 return mid
             else:
                 high = mid-1
     return -1
 
-print(FirstOccurance(x))
+print(FirstOccurance(arr,x))
+print(Firstoccurance(arr,0,5,x))
